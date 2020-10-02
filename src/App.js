0b3d9bbb014df41b4418d99e7f5873b22c22e573
azeemaleem149 <71729035+azeemaleem149@users.noreply.github.com>
2020-10-03 00:45:07 +0500

    COMMIT ROUTE

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import Login from './components/login/login'
-import {ToastContainer} from 'react-toastify';
-import Register from './components/register/register';
-import Navbar from "./components/common/navbar";
-import {Switch,Route,Redirect} from "react-router-dom";
-import "react-toastify/dist/ReactToastify.css";
-import Logout from "./components/logout/logout";
-import ProtectedRoutes from './components/HOC/protectedRoutes';
+// import Login from './components/login/login'
+// import {ToastContainer} from 'react-toastify';
+// import Register from './components/register/register';
+// import Navbar from "./components/common/navbar";
+// import {Switch,Route,Redirect} from "react-router-dom";
+// import "react-toastify/dist/ReactToastify.css";
+// import Logout from "./components/logout/logout";
+// import ProtectedRoutes from './components/HOC/protectedRoutes';
 
 const App = () => {
   return ( 
     <React.Fragment>
-      <ToastContainer/>
+      {/* <ToastContainer/>
       <Switch>
         <ProtectedRoutes path='/' component={Navbar} exact={true}/>
         <Route path="/login" component={Login}/>
         <Route path="/register" component={Register} />
         <Route path="/logout" component={Logout} />
         <Redirect from='/' exact to="/navbar"/>
-      </Switch>
+      </Switch> */}
     </React.Fragment> );
 }
  
