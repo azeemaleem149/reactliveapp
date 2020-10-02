32c4fa768e53045f717cf6a8138ed5f4d9c63229
azeemaleem149 <71729035+azeemaleem149@users.noreply.github.com>
2020-10-03 00:12:51 +0500

    commit changes

--- src/components/common/tableComponent.jsx
+++ src/components/common/tableComponent.jsx
@@ -7,78 +7,78 @@ import { Table } from 'antd';
 const TableComponent = () => {
 
   // All the state variables here 
-  const [show,setShow]=useState(false);
-  const [loading,setLoading]=useState(false);
-  const [name,setName]=useState("");
-  const [email,setEmail]=useState("");
-  const [phone,setPhone]=useState("");
-  const [password,setPassword]=useState("");
+//   const [show,setShow]=useState(false);
+//   const [loading,setLoading]=useState(false);
+//   const [name,setName]=useState("");
+//   const [email,setEmail]=useState("");
+//   const [phone,setPhone]=useState("");
+//   const [password,setPassword]=useState("");
 
-  useEffect(()=>{
-    if(loading){
-      setTimeout(() => {
-        setLoading(false)
-      }, 1500);
-    }
-  },[loading]);
+//   useEffect(()=>{
+//     if(loading){
+//       setTimeout(() => {
+//         setLoading(false)
+//       }, 1500);
+//     }
+//   },[loading]);
 
-const helloHandeler=()=>{
-    setLoading(!loading);
-    setTimeout(()=>{
-      setLoading(!loading);
-      setShow(!show);
-    },1500);
-    // Geting Data from Local storage 
-    getDetails(); 
-  };
-  if (loading) return <BeatLoader/>
+// const helloHandeler=()=>{
+//     setLoading(!loading);
+//     setTimeout(()=>{
+//       setLoading(!loading);
+//       setShow(!show);
+//     },1500);
+//     // Geting Data from Local storage 
+//     getDetails(); 
+//   };
+//   if (loading) return <BeatLoader/>
 
-  const getDetails=()=>{
-    let allValues=localStorage.getItem('values');
-    let getValues=JSON.parse(allValues);
-    setName(getValues.nickname);
-    setEmail(getValues.email);
-    setPhone(getValues.phone);
-    setPassword(getValues.password);
+//   const getDetails=()=>{
+//     let allValues=localStorage.getItem('values');
+//     let getValues=JSON.parse(allValues);
+//     setName(getValues.nickname);
+//     setEmail(getValues.email);
+//     setPhone(getValues.phone);
+//     setPassword(getValues.password);
   
 
-  }
+//   }
 
-  // Table Column Data 
-    const columns = [
-        {
-          title: 'Name',
-          dataIndex: 'name',
-        },
-        {
-          title: 'Email',
-          dataIndex: 'email',
-        },
-        {
-          title: 'Phone',
-          dataIndex: 'phone',
-        },
-        {
-          title: 'Password',
-          dataIndex: 'password',
-        },
-      ];
+//   // Table Column Data 
+//     const columns = [
+//         {
+//           title: 'Name',
+//           dataIndex: 'name',
+//         },
+//         {
+//           title: 'Email',
+//           dataIndex: 'email',
+//         },
+//         {
+//           title: 'Phone',
+//           dataIndex: 'phone',
+//         },
+//         {
+//           title: 'Password',
+//           dataIndex: 'password',
+//         },
+//       ];
 
-      // Fething Data From State 
-      let data = [
-        {
-          key: '1',
-          name,
-          email,
-          phone,
-          password,
-        },
-      ];
+//       // Fething Data From State 
+//       let data = [
+//         {
+//           key: '1',
+//           name,
+//           email,
+//           phone,
+//           password,
+//         },
+//       ];
       
     return ( 
         <div>
-            <Table columns={columns} dataSource={data} size="small" />
-            <Button type="primary" onClick={helloHandeler}>View Details</Button>
+            {/* <Table columns={columns} dataSource={data} size="small" /> */}
+            <Button type="primary" >View Details</Button>
         </div>
      );
 }
