dc15039ed420f1741a411e611a063049594160ec
azeemaleem149 <71729035+azeemaleem149@users.noreply.github.com>
2020-10-03 00:22:44 +0500

    register

--- src/App.js
+++ src/App.js
@@ -25,8 +25,8 @@ const App = () => {
           return <Navbar {...props}/>;
         }}/> */}
         <ProtectedRoutes path='/' component={Navbar} exact={true}/>
-        {/* <Route path="/login" component={Login}/> */}
-        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login}/>
+        {/* <Route path="/register" component={Register} /> */}
         <Route path="/logout" component={Logout} />
         <Redirect from='/' exact to="/navbar"/>
       </Switch>
