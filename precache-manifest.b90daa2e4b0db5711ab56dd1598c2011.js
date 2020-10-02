9fdfa96b39204519c493e8b79e4819bdc8d62df3
Muhammad Azeem <muhammad.azeem@datum.lahore>
2020-10-02 19:26:46 +0500

    Updates

---
+++ precache-manifest.b90daa2e4b0db5711ab56dd1598c2011.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8449159362e0b3f402a9bdc61a7e493",
+    "url": "/reactliveapp/index.html"
+  },
+  {
+    "revision": "f0530a7084791c07b5af",
+    "url": "/reactliveapp/static/css/2.04d85060.chunk.css"
+  },
+  {
+    "revision": "3cdfb2ef220a6d9164e4",
+    "url": "/reactliveapp/static/css/main.b7aae3ab.chunk.css"
+  },
+  {
+    "revision": "f0530a7084791c07b5af",
+    "url": "/reactliveapp/static/js/2.2e75d65e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactliveapp/static/js/2.2e75d65e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cdfb2ef220a6d9164e4",
+    "url": "/reactliveapp/static/js/main.37bbc8d8.chunk.js"
+  },
+  {
+    "revision": "9d91de9bc7c91a077189",
+    "url": "/reactliveapp/static/js/runtime-main.5fc8f0ba.js"
+  }
+]);
