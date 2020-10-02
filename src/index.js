c00050435fa1d50fdf4567434ff2146a45e5d8b1
azeemaleem149 <71729035+azeemaleem149@users.noreply.github.com>
2020-10-03 02:18:03 +0500

    root

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.render(
        <App />
     </BrowserRouter>,
   /* </React.StrictMode>, */
-  document.getElementById('root')
+  document.getElementById('root1')
 );
 
 // If you want your app to work offline and load faster, you can change
