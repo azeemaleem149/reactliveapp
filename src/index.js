6af2abe18f1240bcc46a0d22a34a9603a8a4da0c
azeemaleem149 <71729035+azeemaleem149@users.noreply.github.com>
2020-10-05 19:12:02 +0500

    browser route change

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
    <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/reactliveapp">
        <App />
     </BrowserRouter>
   </React.StrictMode>, 
