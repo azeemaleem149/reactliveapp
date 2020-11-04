80a86ac49088d700d335ffcde2d62d92c2fe1cf9
azeemaleem149 <71729035+azeemaleem149@users.noreply.github.com>
2020-11-04 19:14:36 +0500

    COMMIT
    COMMIT

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,95 +1,99 @@
-import React from 'react';
-import { Form, Input, Button, Checkbox } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import {toast} from 'react-toastify';
-import {Link} from "react-router-dom"
+import React from "react";
+import { Form, Input, Button, Checkbox } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
 import "./login.scss";
 
-// React Toastify Error 
-const error=()=>{
-  toast.error ("Please Correct your Information");
-}
+// React Toastify Error
+const error = () => {
+  toast.error("Please Correct your Information");
+};
 
-// Login Component Start from here 
+// Login Component Start from here
 const Login = (props) => {
   const onFinish = (values) => {
-    let entry=localStorage.getItem('values');   /*  Fetching data from local storage */
-    let entryJson=JSON.parse(entry);
+    let entry = localStorage.getItem(
+      "values"
+    ); /*  Fetching data from local storage */
+    let entryJson = JSON.parse(entry);
 
-    let storageUsername=entryJson.email;
-    let storagePassword=entryJson.password;
+    let storageUsername = entryJson.email;
+    let storagePassword = entryJson.password;
 
-// Validate registered User 
-  
-      values.username===storageUsername && values.password===storagePassword ? localStorage.setItem('auhenticate',true): error();
-    
-    values.username===storageUsername && values.password===storagePassword && props.history.push('/')
-    
+    // Validate registered User
 
-    }
-  return ( 
+    values.username === storageUsername && values.password === storagePassword
+      ? localStorage.setItem("auhenticate", true)
+      : error();
+
+    values.username === storageUsername &&
+      values.password === storagePassword &&
+      props.history.push("/");
+  };
+  return (
     /*  Login Form Start here*/
 
-    <div id='loginForm'>
-  <Form
-    name="normal_login"
-    className="login-form"
-    initialValues={{
-      remember: true,
-    }}
-    onFinish={onFinish} >
+    <div id="loginForm">
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Username!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Password!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
 
-    <Form.Item
-      name="username"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your Username!',
-        },
-      ]} 
-    >
-      <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-    </Form.Item>
-    <Form.Item
-      name="password"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your Password!',
-        },
-      ]}    
-    >
-      <Input
-        prefix={<LockOutlined className="site-form-item-icon" />}
-        type="password"
-        placeholder="Password"
-      />
-    </Form.Item>
-    <Form.Item>
-      <Form.Item name="remember" valuePropName="checked" noStyle>
-        <Checkbox >Remember me</Checkbox>
-      </Form.Item>
+          <a className="login-form-forgot" href="/">
+            Forgot password
+          </a>
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button>
+          Or <Link to="./register">register now!</Link>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
 
-      <a className="login-form-forgot" href="/">
-        Forgot password
-      </a>
-    </Form.Item>
-    <Form.Item>
-      <Button type="primary" htmlType="submit" className="login-form-button">
-       Log in
-      </Button>
-      Or <Link to="./register">register now!</Link>
-    </Form.Item>
-  </Form>
-      </div>
-   );
-}
- 
 export default Login;
-
-
-
-
-
-
-
