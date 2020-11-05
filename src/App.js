8619f95c39f4fd8f0b748719b23984949ef23001
azeemaleem149 <71729035+azeemaleem149@users.noreply.github.com>
2020-11-05 23:24:59 +0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
-import React from 'react';
-import Login from './components/login/login'
-import {ToastContainer} from 'react-toastify';
-import Register from './components/register/register';
-import Home from './components/Home/home';
-import {Switch,Route,Redirect} from "react-router-dom";
+import React from "react";
+import Login from "./components/login/login";
+import { ToastContainer } from "react-toastify";
+import Register from "./components/register/register";
+import Home from "./components/Home/home";
+import { Switch, Route, Redirect } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import Logout from "./components/logout/logout";
-import ProtectedRoutes from './components/HOC/protectedRoutes';
-import UserDetail from './components/user detail/userDetail';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ProtectedRoutes from "./components/HOC/protectedRoutes";
+import UserDetail from "./components/user detail/userDetail";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  return ( 
+  return (
     <React.Fragment>
-       <ToastContainer/>
+      <ToastContainer />
       <Switch>
-        <ProtectedRoutes path='/' component={Home} exact={true}/>
-        <Route path="/login" component={Login}/>
+        <ProtectedRoutes path="/" component={Home} exact={true} />
+        <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
         <Route path="/logout" component={Logout} />
         <Route path="/userdetail" component={UserDetail} />
-        <Redirect from='/' exact to="/home"/>
-      </Switch> 
-    </React.Fragment> );
-}
- 
+        <Redirect from="/" exact to="/home" />
+      </Switch>
+    </React.Fragment>
+  );
+};
+
 export default App;
-
-
-
-    
-
-
