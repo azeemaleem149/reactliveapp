2bc20e6e68e66f0a2d9099678eb8cb188dd5fcf2
azeemaleem149 <71729035+azeemaleem149@users.noreply.github.com>
2020-11-05 23:39:39 +0500

    Remove commented code

--- src/helper.js
+++ src/helper.js
@@ -1,14 +1,3 @@
-// const Helper1 = () => {
-//   const user = localStorage.getItem("auhenticate");
-//   return user;
-// };
-// const Helper2 = () => {
-//   let previousData = JSON.parse(localStorage.getItem("previousData"));
-//   return previousData;
-// };
-
-// export { Helper1, Helper2 };
-
 const Helper = (value) => {
   switch (value) {
     case 1:
